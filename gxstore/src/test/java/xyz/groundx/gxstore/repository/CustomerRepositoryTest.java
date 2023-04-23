049124a847d2b51d73119b353a01ef102a1cafd6
jordan.jung <jordan.jung@groundx.xyz>
2023-04-23 14:44:28 +0900

    gxstore - Customer(5)

--- gxstore/src/test/java/xyz/groundx/gxstore/repository/CustomerRepositoryTest.java
+++ gxstore/src/test/java/xyz/groundx/gxstore/repository/CustomerRepositoryTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 import xyz.groundx.gxstore.model.Customer;
 
 import java.util.List;

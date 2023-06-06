6a411c53f9c8e97fd83321b61b38cd528dec804b
jordan.jung <jordan.jung@groundx.xyz>
2023-06-07 08:30:22 +0900

    ready-to-apidoc

--- gxstore/src/main/java/xyz/groundx/gxstore/GxstoreApplication.java
+++ gxstore/src/main/java/xyz/groundx/gxstore/GxstoreApplication.java
@@ -2,35 +2,18 @@ package xyz.groundx.gxstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.context.MessageSourceProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.query.FluentQuery;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.util.StringUtils;
-import xyz.groundx.gxstore.model.Customer;
-import xyz.groundx.gxstore.repository.CustomerRepository;
-
-import java.time.Duration;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
 
 @SpringBootApplication
 @EnableCaching
 @EnableJpaAuditing
 public class GxstoreApplication {
+    static {
+        new FixedHostPortGenericContainer<>("redis:5.0.3-alpine").withFixedExposedPort(6379, 16379);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(GxstoreApplication.class, args);
     }
---
+++ gxstore/src/main/java/xyz/groundx/gxstore/SpringdocConfig.java
@@ -0,0 +1,9 @@
+package xyz.groundx.gxstore;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication
+public class SpringdocConfig {
+}
